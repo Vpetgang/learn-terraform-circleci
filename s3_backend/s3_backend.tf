cd82397f8a1c92aab540ed4048f960dd5a60e920
Petgang, Victoire <vpetgang@ezesoft.com>
2022-01-10 11:35:44 -0500

    s3_backend.tf

--- s3_backend/s3_backend.tf
+++ s3_backend/s3_backend.tf
@@ -9,10 +9,11 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  profile = "vpetgangtest"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket_prefix = "circle-ci-backend-"
+resource "aws_s3_bucket" "s3_bucket_terraform_state" {
+  bucket_prefix = "circle-ci-backend-001-"
   # Enable versioning so we can see the full revision history of our
   # state files
   force_destroy = true
@@ -30,5 +31,30 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 output "s3_bucket_name" {
-  value = aws_s3_bucket.terraform_state.bucket
+  value = aws_s3_bucket.s3_bucket_terraform_state.bucket
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
