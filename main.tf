70dacabb3c4acc15cfa44db8724be6cd49fe33eb
Petgang, Victoire <vpetgang@ezesoft.com>
2022-01-10 00:20:16 -0500

    main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,11 @@ terraform {
     }
   }
   required_version = "> 0.14"
-}
-
-backend "s3" {
-  bucket = "circle-ci-backend-001-20220110044538657000000001"
-  key    = "terraform/webapp/terraform.tfstate"
-  region = "us-east-1"
+  backend "s3" {
+    bucket = "circle-ci-backend-001-20220110044538657000000001"
+    key    = "terraform/webapp/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
